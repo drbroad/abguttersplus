1ea1e78dd08aeff9f4f16965aa008ef4f8e38c2a
responsemktg <marcbroad@Marcs-MacBook-Pro-M1.local>
2024-06-30 21:50:54 -0400

    initial removal of crap

--- wp-content/plugins/gp-premium/dist/dashboard.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0ab64c3a1832f10e060d');
--- wp-content/plugins/gp-premium/dist/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-edit-post', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-plugins'), 'version' => 'd1ae659d7b937f65b15d');
